ffc6bc83ebb338c56f12c54bdc3a0789e5c8dff0
nk <iamnk1433@gmail.com>
2022-02-11 17:49:43 +0530

    Demo.

---
+++ config.js
@@ -0,0 +1,19 @@
+const config = {
+	name: 'trial',
+	template: 'react-starter',
+	target: 'git@github.com:Laufire/demo.git',
+	content: [
+		{
+			type: 'button',
+			name: 'buttonOne',
+			content: 'buttonOne',
+		},
+		{
+			type: 'button',
+			name: 'buttonTwo',
+			content: 'buttonTwo',
+		},
+	],
+};
+
+module.exports = config;
