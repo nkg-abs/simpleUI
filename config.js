db37b315157fdd04aa351aec1f83b9cc7eff64c9
nkg-abs <nkg@abs.in>
2023-11-16 12:05:26 +0530

    Improved.

--- config.js
+++ config.js
@@ -2,18 +2,36 @@ const config = {
 	name: 'trial',
 	template: 'react-starter',
 	target: 'git@github.com:Laufire/demo.git',
-	content: [
-		{
-			type: 'button',
-			name: 'buttonOne',
-			content: 'buttonOne',
+	theme: 'material',
+	customizations: {
+		components: {
+			type: 'function',
 		},
-		{
-			type: 'button',
-			name: 'buttonTwo',
-			content: 'buttonTwo',
+		services: {
+			type: 'function',
 		},
-	],
+	},
+	content: {
+		panelData: {
+			type: 'simple',
+			data: "click",
+		},
+		panel: {
+			type: 'button',
+			data: 'panelData'
+		},
+		app: {
+			type: 'box',
+			props: {
+				values: 'getFiles',
+				width: 100,
+			},
+			content: 'context.state.app',
+		},
+	},
+	dependencies: {
+		"glob": "^8.0.1",
+	},
 };
 
 module.exports = config;
